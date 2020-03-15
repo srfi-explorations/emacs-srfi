fa5d8aea3d08ec67d9540a74e9fd02b6d3be066a
Lassi Kortela <lassi@lassi.io>
2020-03-15 22:08:22 +0200

    Drop cl dependency in converter
    cl-destructuring-bind is unnecessary

--- srfi-data-convert.el
+++ srfi-data-convert.el
@@ -6,21 +6,6 @@
           (while t (setq forms (nconc forms (list (read (current-buffer))))))
         (end-of-file forms)))))
 
-(defun srfi-data-convert--convert (original-srfis)
-  (let ((next-number 0))
-    (mapcar
-     (lambda (srfi)
-       (let* ((number (cadr (assoc 'number srfi)))
-              (status (cadr (assoc 'status srfi)))
-              (title  (cadr (assoc 'title  srfi)))
-              (year   (and (not (equal 'draft status))
-                           (let ((date (cadr (assoc 'done-date srfi))))
-                             (string-to-number (substring date 0 4))))))
-         (unless (equal number next-number) (error "Bad numbering"))
-         (setq next-number (1+ next-number))
-         (list number year status title)))
-     original-srfis)))
-
 (defun srfi-data-convert--write-all (srfis)
   (with-temp-buffer
     (insert
@@ -35,11 +20,20 @@
      "\n"
      "(defconst srfi-data" "\n"
      "  [\n")
-    (mapc (lambda (srfi)
-            (cl-destructuring-bind (number year status title) srfi
-              (insert (format "   ;; SRFI %d\n" number)
-                      (format "   %S %S %S\n" year status title))))
-          srfis)
+    (let ((next-number 0))
+      (mapc (lambda (srfi)
+              (let* ((number (cadr (assoc 'number srfi)))
+                     (status (cadr (assoc 'status srfi)))
+                     (title  (cadr (assoc 'title  srfi)))
+                     (year   (and (not (equal 'draft status))
+                                  (let ((date (cadr (assoc 'done-date srfi))))
+                                    (string-to-number
+                                     (substring date 0 4))))))
+                (unless (equal number next-number) (error "Bad numbering"))
+                (setq next-number (1+ next-number))
+                (insert (format "   ;; SRFI %d\n" number)
+                        (format "   %S %S %S\n" year status title))))
+            srfis))
     (insert "   ]" "\n"
             "  \"Table of all known SRFI documents.\")" "\n"
             "\n"
@@ -50,6 +44,4 @@
 
 (defun srfi-data-convert ()
   (interactive)
-  (srfi-data-convert--write-all
-   (srfi-data-convert--convert
-    (srfi-data-convert--read-all))))
+  (srfi-data-convert--write-all (srfi-data-convert--read-all)))
