c11ad93fafac987c88374db055dff1a8aac538aa
Lassi Kortela <lassi@lassi.io>
2020-03-31 11:46:35 +0300

    Merge admin scripts into one
    Now there's just admin/srfi-data.sh. It downloads the latest data,
    converts it into Emacs Lisp, and commits the update to git.

--- admin/srfi-data-convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-cd "$(dirname "$0")"
-echo "Entering directory '$PWD'"
-set -x
-${EMACS:-emacs} -Q -batch -L . -l srfi-data-convert -f srfi-data-convert
-mv -f srfi-data.el.new ../srfi-data.el
--- admin/srfi-data.sh
+++ admin/srfi-data.sh
@@ -4,3 +4,7 @@ cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 set -x
 curl --fail --silent --show-error -o srfi-data.scm https://raw.githubusercontent.com/scheme-requests-for-implementation/srfi-common/master/admin/srfi-data.scm
+${EMACS:-emacs} -Q -batch -L . -l srfi-data-convert -f srfi-data-convert
+mv -f srfi-data.el.new ../srfi-data.el
+git commit -m "Update SRFI data" ../srfi-data.el
+git show
