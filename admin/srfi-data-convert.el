776592c3a707a862938776f0a9266c7bad26aebe
Arthur A. Gleckler <aag@alum.mit.edu>
2020-11-29 12:20:40 -0800

    Hyphenate.

--- admin/srfi-data-convert.el
+++ admin/srfi-data-convert.el
@@ -22,7 +22,7 @@
     (lazy-evaluation "Lazy Evaluation")
     (miscellaneous "Miscellaneous")
     (modules "Modules")
-    (multiple-value-returns "Multiple Value Returns")
+    (multiple-value-returns "Multiple-Value Returns")
     (numbers "Numbers")
     (operating-system "Operating System")
     (optimization "Optimization")
