633a0ff419438987f6271ff5a5da26307950a3cd
Arthur A. Gleckler <aag@alum.mit.edu>
2021-03-24 21:45:34 -0700

    Update SRFI data

--- srfi-data.el
+++ srfi-data.el
@@ -459,6 +459,8 @@
    nil draft "Compound Objects"
    ;; SRFI 223:
    nil draft "Generalized binary search procedures"
+   ;; SRFI 224:
+   nil draft "Integer Mappings"
    ]
   "Table of all known SRFI documents.")
 
@@ -484,7 +486,7 @@
      57 60 63 66 69 70 74 76 84 86 90 99 100 101 111 113
      115 116 117 118 121 122 124 125 126 127 130 131 133 134 135 136
      137 140 142 146 150 151 152 153 155 158 160 161 164 171 174 175
-     178 179 182 184 185 189 195 196 208 209 214 217 221 222)
+     178 179 182 184 185 189 195 196 208 209 214 217 221 222 224)
     ("Error Handling"
      23 198 199)
     ("Exceptions"
