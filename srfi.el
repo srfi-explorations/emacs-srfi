ce69158f5efb3040a00e89ffef0d1931e5038f53
Lassi Kortela <lassi@lassi.io>
2021-05-04 09:32:04 +0300

    Avoid blank lines in header comments

--- srfi.el
+++ srfi.el
@@ -1,33 +1,34 @@
 ;;; srfi.el --- Scheme Requests for Implementation browser -*- lexical-binding: t -*-
-;;
+
 ;; Copyright 2019, 2020, 2021 Lassi Kortela
 ;; Copyright 2020 Arthur A. Gleckler
 ;; SPDX-License-Identifier: MIT
+
 ;; Author: Lassi Kortela <lassi@lassi.io>
 ;; URL: https://github.com/srfi-explorations/emacs-srfi
 ;; Version: 0.3.0
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: languages util
-;;
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Provides quick access to Scheme Requests for Implementation (SRFI)
 ;; documents from within Emacs:
-;;
+
 ;; * `M-x srfi-list` brings up a *SRFI* buffer listing all SRFIs.
-;;
+
 ;; * `M-x srfi` does the same, but lets you live-narrow the list by
 ;;   typing numbers or words into the minibuffer.
-;;
+
 ;; * The *SRFI* buffer provides one-key commands to visit the SRFI
 ;;   document, its discussion (mailing list) archive, and its version
 ;;   control repository. These commands open the right web pages in
 ;;   your web browser using `browse-url'. The standard variable
 ;;   `browse-url-browser-function' can be used to control which web
 ;;   browser is used to open what pages.
-;;
+
 ;;; Code:
 
 (require 'browse-url)
