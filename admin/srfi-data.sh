a04b9d99291cf73afa1ae7ddceed51e3455fffd6
Arthur A. Gleckler <aag@alum.mit.edu>
2026-09-16 20:52:16 -0700

    Do nothing if "srfi-data.el" hasn't changed.

--- admin/srfi-data.sh
+++ admin/srfi-data.sh
@@ -6,5 +6,7 @@ set -x
 curl --fail --silent --show-error -o srfi-data.scm https://raw.githubusercontent.com/scheme-requests-for-implementation/srfi-common/master/admin/srfi-data.scm
 ${EMACS:-emacs} -Q -batch -L . -l srfi-data-convert -f srfi-data-convert
 mv -f srfi-data.el.new ../srfi-data.el
-git commit --message "Update SRFI data" ../srfi-data.el
-git --no-pager show
+if ! git diff --quiet ../srfi-data.el; then
+    git commit --message "Update SRFI data" ../srfi-data.el
+    git --no-pager show
+fi
